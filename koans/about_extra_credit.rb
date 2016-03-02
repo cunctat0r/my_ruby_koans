d89a8978a1b45f39594645ac49c312a114515f96
Slava Petrov <slavik--petrov@yandex.ru>
2016-03-02 20:10:30 +0300

    Dice game: beginning

--- koans/about_extra_credit.rb
+++ koans/about_extra_credit.rb
@@ -6,3 +6,62 @@
 # You already have a DiceSet class and score function you can use.
 # Write a player class and a Game class to complete the project.  This
 # is a free form assignment, so approach it however you desire.
+class DiceSet
+  def initialize
+    @values = []
+  end
+  
+
+  def roll(num_of_rolls)
+    @values = []    
+    num_of_rolls.times do
+      @values << (1 + rand(6))
+    end
+    @values
+  end
+
+  def values
+    @values
+  end
+
+end
+
+def score(dice)
+   
+  score = 0
+
+  rolls = dice.inject(Hash.new(0))  do |result, value|
+    result[value] += 1
+    result
+  end
+
+  rolls.each do |key, value|
+    multipliers = value.divmod(3)    
+    
+    if key == 1
+      three_value = 1000
+      single_value = 100
+    elsif key == 5
+      three_value = 500
+      single_value = 50
+    else
+      three_value = 100 * key
+      single_value = 0
+    end
+    score += (three_value * multipliers[0] + single_value * multipliers[1])     
+        
+  end  
+
+  score
+
+end
+
+class Player
+	def initialize
+	end
+end
+
+class Game
+	def initialize
+	end
+end
